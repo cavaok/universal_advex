c21b02dfcfd1265bdf7e96de0e00ef6315742f95
Olivia Cava <okcava@login-01.cm.cluster>
2025-01-01 14:39:49 -0500

    mlp models trained

--- trainers/trainer.sh
+++ trainers/trainer.sh
@@ -2,12 +2,12 @@
 #SBATCH -N 1                  # Number of nodes
 #SBATCH -n 1                  # Number of tasks
 #SBATCH --mem=16G             # Memory per node
-#SBATCH -t 1:00:00            # Time required
+#SBATCH -t 3:00:00            # Time required
 #SBATCH -p short              # Partition
 #SBATCH -J auto_sweeps        # Job name
 #SBATCH -o sweep_out.txt      # Standard output
 #SBATCH -e sweep_err.txt      # Standard error
-#SBATCH --gres=gpu:A100:1     # Request 1 A100 GPU
+# SLURM --gres=gpu:A100:1     # Request 1 A100 GPU
 
 # Load Python environment
 module load python/3.8.13/slu6jvw
