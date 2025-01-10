2836cae22c81a5c152c4dad77047a4c1d0122980
Olivia Cava <okcava@login-01.cm.cluster>
2025-01-10 03:00:02 -0500

    successful

--- trainer.sh
+++ trainer.sh
@@ -7,7 +7,7 @@
 #SBATCH -J adved              # Job name
 #SBATCH -o advex_out.txt      # Standard output
 #SBATCH -e advex_err.txt      # Standard error
-#SBATCH --gres=gpu:A100:1     # Request 1 A100 GPU
+#SBATCH --gres=gpu:L40S:1     # Request 1 L40S GPU
 
 # Load Python environment
 module load python/3.8.13/slu6jvw
